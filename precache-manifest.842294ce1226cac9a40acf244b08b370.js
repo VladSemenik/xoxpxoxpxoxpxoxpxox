ff2489e7177313920683ad9da06a79c80720a914
VladSemenik <vlad.semenik@gmail.com>
2019-02-24 20:10:01 +0300

    add: app dashboard-mentor

---
+++ precache-manifest.842294ce1226cac9a40acf244b08b370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "344e8ebeecdedfaa3e72",
+    "url": "/static/js/main.344e8ebe.chunk.js"
+  },
+  {
+    "revision": "d1f9c2c3ca9aa0166543",
+    "url": "/static/js/2.d1f9c2c3.chunk.js"
+  },
+  {
+    "revision": "344e8ebeecdedfaa3e72",
+    "url": "/static/css/main.2b741d2b.chunk.css"
+  },
+  {
+    "revision": "d1f9c2c3ca9aa0166543",
+    "url": "/static/css/2.096cbc03.chunk.css"
+  },
+  {
+    "revision": "a4984442d3ff64019a91e17d1c53096b",
+    "url": "/index.html"
+  }
+];
